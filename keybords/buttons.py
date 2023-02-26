28ec6277d0beabb23cbec5e1b860186cb65549a0
Artem Shashov <tauquantum@gmail.com>
2023-02-26 13:29:34 +1000

    added category_kb

--- keybords/buttons.py
+++ keybords/buttons.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
@@ -20,3 +20,29 @@ def ok_cancel_kb() -> ReplyKeyboardMarkup:
     kb.button(text="Cancel")
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True, one_time_keyboard=False)
+
+
+def category_kb() -> InlineKeyboardMarkup:
+    buttons = [
+        [InlineKeyboardButton(text="What is South Korea for me",
+                              callback_data="What is South Korea for me")],
+        [InlineKeyboardButton(text="The purest feeling in the life",
+                              callback_data="The purest feeling in the life")],
+        [InlineKeyboardButton(text="Student’s life",
+                              callback_data="Student’s life"),
+         InlineKeyboardButton(text="Urban",
+                              callback_data="Urban")],
+        [InlineKeyboardButton(text="Model photography",
+                              callback_data="Model photography"),
+         InlineKeyboardButton(text="People",
+                              callback_data="People")],
+        [InlineKeyboardButton(text="Creativity",
+                              callback_data="Creativity"),
+         InlineKeyboardButton(text="Nature",
+                              callback_data="Nature")],
+        [InlineKeyboardButton(text="Global citizen",
+                              callback_data="Global citizen")]
+    ]
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard

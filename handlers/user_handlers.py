28eddfad41bb616b484990ba593fe4d5babf3de2
Artem Shashov <tauquantum@gmail.com>
2023-02-27 18:45:35 +1000

    added edit message with callback_query

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -8,12 +8,17 @@ from keybords import buttons
 
 class Loader(StatesGroup):
     upload_state = State()
-    filename_state = State()
+    description_state = State()
     category_state = State()
     location_state = State()
     camera_state = State()
     author_state = State()
     send_state = State()
+    edit_description_state = State()
+    edit_category_state = State()
+    edit_location_state = State()
+    edit_camera_state = State()
+    edit_author_state = State()
 
 
 router = Router()
@@ -21,7 +26,7 @@ router = Router()
 
 @router.message(Command(commands=['start']))
 async def start_command(message: types.Message):
-    await message.answer('Hello! I am PCS_Bot, and I will help you to share your piece of art with people! 😎',
+    await message.answer('Hello! \nI am PCS_Bot, and I will help you to share your piece of art with people! 😎',
                          reply_markup=buttons.upload_photo_kb())
 
 
@@ -32,7 +37,7 @@ async def upload_photo(message: types.Message, state: FSMContext):
 
 
 @router.message(Text(text=['Cancel']))
-async def cancel(message: types.Message, state: FSMContext) -> None:
+async def cancel(message: types.Message, state: FSMContext):
     await state.clear()
     await message.answer('Canceled', reply_markup=buttons.upload_photo_kb())
 
@@ -44,25 +49,25 @@ async def get_photo(message: types.Message, state: FSMContext):
     elif message.photo:
         await state.update_data(file_id=message.photo[-1].file_id, file='photo')
 
-    await state.set_state(Loader.filename_state)
+    await state.set_state(Loader.description_state)
     await message.reply('Great!')
     await message.answer('I think, we should give the name of this piece of art ')
 
 
 @router.message(Loader.upload_state)
-async def incorrectly_file(message: types.Message) -> None:
+async def incorrectly_file(message: types.Message):
     await message.answer('Invalid file. Please try again', reply_markup=buttons.cancel_kb())
 
 
-@router.message(Loader.filename_state)
-async def set_filename(message: types.Message, state: FSMContext):
-    await state.update_data(filename=message.text)
+@router.message(Loader.description_state)
+async def set_description(message: types.Message, state: FSMContext):
+    await state.update_data(description=message.text)
     await state.set_state(Loader.category_state)
     await message.answer('Beautiful!')
     await message.answer('Choose a category from the list below:', reply_markup=buttons.category_kb())
 
 
-@router.callback_query(Loader.category_state, ~F.data.in_({'confirm', 'back'}))
+@router.callback_query(Loader.category_state, ~F.data.in_({'Confirm', 'Back'}))
 async def choose_category(callback: types.CallbackQuery, state: FSMContext):
     await state.update_data(category=callback.data)
     await callback.message.edit_text(text=f'Selected: {callback.data}', reply_markup=buttons.confirm_kb())
@@ -71,11 +76,13 @@ async def choose_category(callback: types.CallbackQuery, state: FSMContext):
 
 @router.callback_query(Loader.category_state)
 async def confirm_category(callback: types.CallbackQuery, state: FSMContext):
-    if callback.data == 'confirm':
+    if callback.data == 'Confirm':
         await state.set_state(Loader.location_state)
-        await callback.message.edit_text('Where did you take a photo?')
+        category = await state.get_data()
+        await callback.message.edit_text(f'Selected: {category["category"]}')
+        await callback.message.answer('Where did you take a photo?')
         await callback.answer()
-    elif callback.data == 'back':
+    elif callback.data == 'Back':
         await callback.message.edit_text(text='Choose a category from the list below:',
                                          reply_markup=buttons.category_kb())
         await callback.answer()
@@ -102,24 +109,28 @@ async def set_camera(message: types.Message, state: FSMContext):
                          reply_markup=buttons.anonymous_kb())
 
 
-@router.callback_query(Loader.author_state)
+@router.callback_query(Loader.author_state, F.data == 'Anonymous author')
 async def set_author_callback(callback: types.CallbackQuery, state: FSMContext):
     await state.update_data(author=callback.data)
     await callback.message.answer('Check the correctness of the data')
 
     data = await state.get_data()
-    text = f'<b>Description</b>: {data["filename"]}' \
+    text = f'<b>Description</b>: {data["description"]}' \
            f'\n<b>Category</b>: {data["category"]}' \
            f'\n<b>Location</b>: {data["location"]}' \
            f'\n<b>Camera</b>: {data["camera"]}' \
            f'\n<b>Artist</b>: {data["author"]}'
+    await state.update_data(text=text)
+
     if data['file'] == 'document':
         await callback.message.answer_document(document=data["file_id"], caption=text)
     elif data['file'] == 'photo':
         await callback.message.answer_photo(photo=data["file_id"], caption=text)
 
     await state.set_state(Loader.send_state)
-    await callback.message.answer('Press Ok to send', reply_markup=buttons.ok_cancel_kb())
+    await callback.message.answer('Press "Ok" to send or press "Edit" to edit message',
+                                  reply_markup=buttons.ok_edit_kb())
+    await callback.answer()
 
 
 @router.message(Loader.author_state)
@@ -128,22 +139,138 @@ async def set_author_message(message: types.Message, state: FSMContext):
     await message.answer('Check the correctness of the data')
 
     data = await state.get_data()
-    text = f'<b>Description</b>: {data["filename"]}' \
+    text = f'<b>Description</b>: {data["description"]}' \
            f'\n<b>Category</b>: {data["category"]}' \
            f'\n<b>Location</b>: {data["location"]}' \
            f'\n<b>Camera</b>: {data["camera"]}' \
            f'\n<b>Artist</b>: {data["author"]}'
+    await state.update_data(text=text)
+
     if data['file'] == 'document':
         await message.answer_document(document=data["file_id"], caption=text)
     elif data['file'] == 'photo':
         await message.answer_photo(photo=data["file_id"], caption=text)
 
     await state.set_state(Loader.send_state)
-    await message.answer('Press Ok to send', reply_markup=buttons.ok_cancel_kb())
+    await message.answer('Press "Ok" to send or press "Edit" to edit message', reply_markup=buttons.ok_edit_kb())
 
 
-@router.message(Loader.send_state)
-async def send_photo_to_channel(message: types.Message, state: FSMContext):
+@router.callback_query(Loader.send_state, F.data == 'Ok')
+async def send_photo_to_channel(callback: types.CallbackQuery, state: FSMContext):
     # await message.answer_document(document=message.document.file_id, caption='some text')
     await state.clear()
-    await message.answer('Done', reply_markup=buttons.upload_photo_kb())
+    await callback.message.edit_text(text='Done')
+    await callback.message.answer('😎', reply_markup=buttons.upload_photo_kb())
+    await callback.answer()
+
+
+@router.callback_query(Loader.send_state, F.data == 'Edit')
+async def edit_message(callback: types.CallbackQuery):
+    await callback.message.edit_text(text='Select change:', reply_markup=buttons.edit_message_kb())
+    await callback.answer()
+
+
+@router.callback_query(F.data == '<< Back')
+async def press_callback_back(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.edit_text('Press "Ok" to send or press "Edit" to edit message',
+                                     reply_markup=buttons.ok_edit_kb())
+    await state.set_state(Loader.send_state)
+    await callback.answer()
+
+
+@router.callback_query(F.data.in_({'Description', 'Category', 'Location', 'Camera', 'Artist'}))
+async def edit_point_message(callback: types.CallbackQuery, state: FSMContext):
+    if callback.data == 'Description':
+        await callback.message.edit_text('Write a description')
+        await state.set_state(Loader.edit_description_state)
+
+    elif callback.data == 'Category':
+        await callback.message.edit_text('Choose a category', reply_markup=buttons.category_kb())
+        await state.set_state(Loader.edit_category_state)
+
+    elif callback.data == 'Location':
+        await callback.message.edit_text('Write a location')
+        await state.set_state(Loader.edit_location_state)
+
+    elif callback.data == 'Camera':
+        await callback.message.edit_text('Write camera')
+        await state.set_state(Loader.edit_camera_state)
+
+    elif callback.data == 'Artist':
+        await callback.message.edit_text('Write the author or press "Anonymous author',
+                                         reply_markup=buttons.anonymous_kb())
+        await state.set_state(Loader.edit_author_state)
+
+    await callback.answer()
+
+
+@router.message(Loader.edit_description_state)
+async def edit_description(message: types.Message, state: FSMContext):
+    await state.update_data(description=message.text)
+    await state.set_state(Loader.send_state)
+    await message.answer('Changed \nSelect new change or press "Back"', reply_markup=buttons.edit_message_kb())
+
+
+@router.callback_query(Loader.edit_category_state, ~F.data.in_({'Confirm', 'Back'}))
+async def edit_category(callback: types.CallbackQuery, state: FSMContext):
+    await state.update_data(category=callback.data)
+    await callback.message.edit_text(text=f'Selected: {callback.data}', reply_markup=buttons.confirm_kb())
+    await callback.answer()
+
+
+@router.callback_query(Loader.edit_category_state)
+async def confirm_category(callback: types.CallbackQuery, state: FSMContext):
+    if callback.data == 'Confirm':
+        category = await state.get_data()
+        await callback.message.edit_text(f'Selected: {category["category"]}', reply_markup=buttons.edit_message_kb())
+        await callback.answer()
+    elif callback.data == 'Back':
+        await callback.message.edit_text(text='Choose a category from the list below:',
+                                         reply_markup=buttons.category_kb())
+        await callback.answer()
+
+
+@router.message(Loader.edit_location_state)
+async def edit_location(message: types.Message, state: FSMContext):
+    await state.update_data(location=message.text)
+    await state.set_state(Loader.send_state)
+    await message.answer('Changed \nSelect new change or press "Back"', reply_markup=buttons.edit_message_kb())
+
+
+@router.message(Loader.edit_camera_state)
+async def edit_camera(message: types.Message, state: FSMContext):
+    await state.update_data(camera=message.text)
+    await state.set_state(Loader.send_state)
+    await message.answer('Changed \nSelect new change or press "Back"', reply_markup=buttons.edit_message_kb())
+
+
+@router.callback_query(Loader.edit_author_state, F.data == 'Anonymous author')
+async def edit_author(callback: types.CallbackQuery, state: FSMContext):
+    await state.update_data(author=callback.data)
+    await state.set_state(Loader.send_state)
+    await callback.message.edit_text('Changed \nSelect new change or press "Back"',
+                                     reply_markup=buttons.edit_message_kb())
+    await callback.answer()
+
+
+@router.message(Loader.edit_author_state)
+async def edit_author(message: types.Message, state: FSMContext):
+    await state.update_data(author=message.text)
+    await state.set_state(Loader.send_state)
+    await message.answer('Changed \nSelect new change or press "Back"', reply_markup=buttons.edit_message_kb())
+
+
+@router.callback_query(F.data == 'Show')
+async def show_message(callback: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    text = f'<b>Description</b>: {data["description"]}' \
+           f'\n<b>Category</b>: {data["category"]}' \
+           f'\n<b>Location</b>: {data["location"]}' \
+           f'\n<b>Camera</b>: {data["camera"]}' \
+           f'\n<b>Artist</b>: {data["author"]}'
+    if data['file'] == 'document':
+        await callback.message.answer_document(document=data["file_id"], caption=text)
+    elif data['file'] == 'photo':
+        await callback.message.answer_photo(photo=data["file_id"], caption=text)
+    await callback.message.answer(text='Select change:', reply_markup=buttons.edit_message_kb())
+    await callback.answer()

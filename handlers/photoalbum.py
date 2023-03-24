3815b7178a727792ee3eaef1934259f5277f9000
Artem Shashov <tauquantum@gmail.com>
2023-03-24 15:30:18 +1000

    added cancel the order and choice payment method

--- handlers/photoalbum.py
+++ handlers/photoalbum.py
@@ -7,7 +7,7 @@ from aiogram.fsm.state import StatesGroup, State
 from dotenv import load_dotenv
 
 from bot import SingleBot
-from keybords import buttons
+from keybords import default_buttons, callback_buttons
 from solbot_db.db_orm import BotDB
 
 
@@ -18,6 +18,8 @@ class UserData(StatesGroup):
     name_state = State()
     email_state = State()
     instagram_state = State()
+    cancel_order_state = State()
+    # payment_state = State()
 
 
 load_dotenv()
@@ -31,7 +33,7 @@ async def press_photo_album(message: types.Message):
            "great way to remember the good old days of your life's chapters. \n\nCurrent price is 18.000 Won for one " \
            "package. We will make album and print your photos and will give them personally to you. \n\nIf you are " \
            "interested, please press the button “Order the Photo Album” "
-    await message.answer(text=text, reply_markup=buttons.order_photo_album_kb())
+    await message.answer(text=text, reply_markup=callback_buttons.order_photo_album_kb())
 
 
 @router.callback_query(F.data == 'Order the Photo Album')
@@ -39,24 +41,32 @@ async def order_photo_album(callback: types.CallbackQuery, state: FSMContext):
     if BotDB().order_exists(callback.from_user.id):
         await callback.message.edit_text(
             text='Sorry, but according to my data, you already applied for Photoalbum, do you want to cancel your '
-                 'request and do it again?', reply_markup=buttons.yes_no_kb())
-
-        # TODO: сделать логику пересоздания заказа
+                 'request and do it again?')
+        await callback.message.answer(text='Нажмите необходимую кнопку', reply_markup=default_buttons.cancel_order_kb())
+        await state.set_state(UserData.cancel_order_state)
 
     else:
-        await callback.message.answer(text='Этап формирования заказа', reply_markup=buttons.cancel_kb())
+        await callback.message.answer(text='Этап формирования заказа', reply_markup=default_buttons.cancel_kb())
 
         if BotDB().user_exists(callback.from_user.id):
             user = BotDB().get_user_info(callback.from_user.id)
             await callback.message.answer(text=f'Сверяем пользовательские данные:'
                                                f'\n\nName: {user.name}'
                                                f'\nEmail: {user.email}'
-                                               f'\nInstagram: {user.instagram}', reply_markup=buttons.change_data_kb())
+                                               f'\nInstagram: {user.instagram}',
+                                          reply_markup=callback_buttons.change_data_kb())
 
         else:
             await callback.message.answer(text='Could you tell me what is your name, please?')
             await state.set_state(UserData.name_state)
     await callback.answer()
+
+
+@router.message(UserData.cancel_order_state, Text(text=['Cancel the order']))
+async def cancel_order(message: types.Message, state: FSMContext):
+    order_id = BotDB().cancel_order(user_id=message.from_user.id)
+    await message.answer(text=f'Заказ №<b>{order_id}</b> отменен', reply_markup=default_buttons.main_menu_kb())
+    await state.clear()
 
 
 @router.message(UserData.name_state)
@@ -86,13 +96,13 @@ async def get_user_name(message: types.Message, state: FSMContext):
     await message.answer(text=f'Сверяем пользовательские данные:'
                               f'\n\nName: {data["name"]}'
                               f'\nEmail: {data["email"]}'
-                              f'\nInstagram: {data["instagram"]}', reply_markup=buttons.change_data_kb())
+                              f'\nInstagram: {data["instagram"]}', reply_markup=callback_buttons.change_data_kb())
     await state.clear()
 
 
 @router.callback_query(F.data == 'Change')
 async def change_user_data(callback: types.CallbackQuery):
-    await callback.message.edit_text(text=f'Selected change', reply_markup=buttons.choose_user_data_kb())
+    await callback.message.edit_text(text=f'Selected change', reply_markup=callback_buttons.choose_user_data_kb())
     await callback.answer()
 
 
@@ -120,7 +130,7 @@ async def change_selected_category(message: types.Message, state: FSMContext):
     await message.answer(text=f'Изменено. Сверяем пользовательские данные:'
                               f'\n\nName: {user.name}'
                               f'\nEmail: {user.email}'
-                              f'\nInstagram: {user.instagram}', reply_markup=buttons.change_data_kb())
+                              f'\nInstagram: {user.instagram}', reply_markup=callback_buttons.change_data_kb())
     await state.clear()
 
 
@@ -130,7 +140,8 @@ async def return_to_selected_category(callback: types.CallbackQuery):
     await callback.message.edit_text(text=f'Сверяем пользовательские данные:'
                                           f'\n\nName: {user.name}'
                                           f'\nEmail: {user.email}'
-                                          f'\nInstagram: {user.instagram}', reply_markup=buttons.change_data_kb())
+                                          f'\nInstagram: {user.instagram}',
+                                     reply_markup=callback_buttons.change_data_kb())
     await callback.answer()
 
 
@@ -145,7 +156,7 @@ async def request_user_url(callback: types.CallbackQuery, state: FSMContext):
 async def get_user_url(message: types.Message, state: FSMContext):
     await state.update_data(url=message.text)
     await state.set_state(UserData.confirm_url_state)
-    await message.answer(text='Проверьте правильность ссылки', reply_markup=buttons.change_url_kb())
+    await message.answer(text='Проверьте правильность ссылки', reply_markup=callback_buttons.change_url_kb())
 
 
 @router.callback_query(UserData.confirm_url_state, F.data.in_({'Next url', 'Change url'}))
@@ -153,11 +164,25 @@ async def confirm_or_change_url(callback: types.CallbackQuery, state: FSMContext
     if callback.data == 'Next url':
         url = await state.get_data()
         order_id = BotDB().create_order(user_id=callback.from_user.id, url=url['url'])
-        await callback.message.edit_text(text=f'Номер заказа: <b>{order_id}</b>')
-        await callback.message.answer(text='👌', reply_markup=buttons.main_menu_kb())
+        await callback.message.edit_text(text=f'Great! Номер заказа: <b>{order_id}</b>'
+                                              f'\n\nRight now, we have to decide how you going to pay for that.',
+                                         reply_markup=callback_buttons.payment_kb())
         await callback.answer()
-        await state.clear()
+        # await state.set_state(UserData.payment_state)
     if callback.data == 'Change url':
         await state.set_state(UserData.url_state)
         await callback.message.edit_text(text='Введите ссылку на файл')
         await callback.answer()
+
+
+@router.callback_query(F.data.in_({'In Cash', 'Bank Account'}))
+async def choose_payment_method(callback: types.CallbackQuery):
+    if callback.data == 'In Cash':
+        await callback.message.edit_text(text='To pay in cash, please contact with @yusungchoi')
+    if callback.data == 'Bank Account':
+        await callback.message.edit_text(text='You can just transfer your money to our photo club bank '
+                                              'account: 64791074681307'
+                                              '\n\nWhen you upload it, please send to @yusungchoi the screenshot of your '
+                                              'transaction.')
+    await callback.message.answer(text='👌', reply_markup=default_buttons.main_menu_kb())
+    await callback.answer()

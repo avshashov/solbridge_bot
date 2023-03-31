3ffb573380b9070193c2ee03e8dd54bdac37efc7
Artem Shashov <tauquantum@gmail.com>
2023-03-31 18:23:56 +1000

    added orders buttons

--- keybords/default_buttons.py
+++ keybords/default_buttons.py
@@ -6,8 +6,47 @@ def main_menu_kb() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text='Upload photo')
     kb.button(text='Photo Album')
-    kb.button(text='Order a book')
+    kb.button(text='PCS Book')
     kb.button(text='Help')
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True, one_time_keyboard=False)
+
+
+def admin_menu_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text='Orders')
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
+
+
+def orders_category_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text='Photo Albums')
+    kb.button(text='Books')
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
+
+
+def photo_album_status_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text='Open')
+    kb.button(text='Closed')
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
+
+
+def order_is_open_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text='Paid')
+    kb.button(text='Unpaid')
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
+
+
+def order_closed_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text='Cancelled')
+    kb.button(text='Completed')
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
 

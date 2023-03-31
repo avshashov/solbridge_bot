825a9c12090fd871fd0b35360c47793f68a7dbfd
Artem Shashov <tauquantum@gmail.com>
2023-03-31 18:29:48 +1000

    Review. Added book order functionality

--- handlers/orders.py
+++ handlers/orders.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from dotenv import load_dotenv
 
+import phrases
 from bot import SingleBot
 from keybords import default_buttons, callback_buttons
 from solbot_db.db_orm import BotDB
@@ -26,21 +27,27 @@ sol_bot = SingleBot()
 router = Router()
 
 
-@router.message(Text(text=['Photo Album']))
-async def press_photo_album(message: types.Message):
-    text = "Currently, we are offering a photo album with 30 of your photos to preserve your memories. This is a " \
-           "great way to remember the good old days of your life's chapters. \n\nCurrent price is 18.000 Won for one " \
-           "package. We will make album and print your photos and will give them personally to you. \n\nIf you are " \
-           "interested, please press the button “Order the Photo Album” "
-    await message.answer(text=text, reply_markup=callback_buttons.order_photo_album_kb())
+@router.message(F.text.in_({'Photo Album', 'PCS Book'}))
+async def press_photoalbum_or_book(message: types.Message, state: FSMContext):
+    if message.text == 'Photo Album':
+        text = phrases.order_phrases['album']
+        await message.answer(text=text, reply_markup=callback_buttons.order_photo_album_kb())
+    elif message.text == 'PCS Book':
+        text = phrases.order_phrases['book']
+        await message.answer(text=text, reply_markup=callback_buttons.order_book_kb())
 
 
-@router.callback_query(F.data == 'Order the Photo Album')
+@router.callback_query(F.data.in_({'album', 'book'}))
 async def order_photo_album(callback: types.CallbackQuery, state: FSMContext):
-    if BotDB().order_exists(callback.from_user.id):
-        await callback.message.edit_text(
-            text='Sorry, but according to my data, you already applied for Photoalbum, do you want to cancel your '
-                 'request and do it again?')
+    await state.update_data(product=callback.data)
+
+    if callback.data == 'album' and BotDB().order_exists(callback.from_user.id, product='album'):
+        await callback.message.edit_text(text=phrases.order_phrases['exists_album'])
+        await callback.message.answer(text='Choose an action', reply_markup=default_buttons.cancel_order_kb())
+        await state.set_state(UserData.cancel_order_state)
+
+    elif callback.data == 'book' and BotDB().order_exists(callback.from_user.id, product='book'):
+        await callback.message.edit_text(text=phrases.order_phrases['exists_book'])
         await callback.message.answer(text='Choose an action', reply_markup=default_buttons.cancel_order_kb())
         await state.set_state(UserData.cancel_order_state)
 
@@ -61,10 +68,11 @@ async def order_photo_album(callback: types.CallbackQuery, state: FSMContext):
 
 @router.message(UserData.cancel_order_state, Text(text=['Undo Purchase']))
 async def cancel_order(message: types.Message, state: FSMContext):
-    order_id = BotDB().cancel_order(user_id=message.from_user.id)
-    text = BotDB().order_message_for_admin(order_id=order_id)
+    product = await state.get_data()
+    order_id = BotDB().cancel_order(user_id=message.from_user.id, product=product['product'])
+    text = BotDB().order_message_for_admin(order_id=order_id, product=product['product'])
 
-    await sol_bot.send_message(chat_id=os.getenv('GROUP_ID'), text=f'[CANCELLED]'
+    await sol_bot.send_message(chat_id=os.getenv('GROUP_ID'), text=f'<b>[CANCELLED]</b>'
                                                                    f'\n{text}')
     await message.answer(text=f'Order №<b>{order_id}</b> is cancelled', reply_markup=default_buttons.main_menu_kb())
     await state.clear()
@@ -90,7 +98,6 @@ async def get_user_name(message: types.Message, state: FSMContext):
 @router.message(UserData.instagram_state)
 async def get_user_name(message: types.Message, state: FSMContext):
     await state.update_data(instagram=message.text)
-    await state.set_state(UserData.instagram_state)
     await message.reply(text='Thanks')
     data = await state.get_data()
     BotDB().create_user(user_id=message.from_user.id, name=data['name'], username=message.from_user.username,
@@ -98,7 +105,6 @@ async def get_user_name(message: types.Message, state: FSMContext):
     await message.answer(text=f'Please check all your information:'
                               f'{text_user_info(data["name"], data["email"], data["instagram"])}',
                          reply_markup=callback_buttons.change_data_kb())
-    await state.clear()
 
 
 @router.callback_query(F.data == 'Change')
@@ -107,7 +113,7 @@ async def change_user_data(callback: types.CallbackQuery):
     await callback.answer()
 
 
-@router.callback_query(F.data.in_({'Name', 'Email', 'Instagram'}))
+@router.callback_query(F.data.in_({'name', 'email', 'instagram'}))
 async def choose_category(callback: types.CallbackQuery, state: FSMContext):
     await state.set_state(UserData.data_state)
     await state.update_data(category=callback.data)
@@ -119,19 +125,14 @@ async def choose_category(callback: types.CallbackQuery, state: FSMContext):
 async def change_selected_category(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     value = message.text
-    date = await state.get_data()
-    if date['category'] == 'Name':
-        BotDB().change_user_data(user_id=user_id, name=value)
-    if date['category'] == 'Email':
-        BotDB().change_user_data(user_id=user_id, email=value)
-    if date['category'] == 'Instagram':
-        BotDB().change_user_data(user_id=user_id, instagram=value)
+    data = await state.get_data()
+    kwargs = {data['category']: value}
+    BotDB().change_user_data(user_id=user_id, **kwargs)
 
     user = BotDB().get_user_info(message.from_user.id)
     await message.answer(text=f'Changed. Please check all your information:'
                               f'{text_user_info(user.name, user.email, user.instagram)}',
                          reply_markup=callback_buttons.change_data_kb())
-    await state.clear()
 
 
 @router.callback_query(F.data == 'Back change')
@@ -144,10 +145,23 @@ async def return_to_selected_category(callback: types.CallbackQuery):
 
 
 @router.callback_query(F.data == 'Next')
-async def request_user_url(callback: types.CallbackQuery, state: FSMContext):
-    await state.set_state(UserData.url_state)
-    await callback.message.edit_text(text='Right now, could you send me the public link '
-                                          'for your Google Drive with your photos (Maximum 30 photos).')
+async def request_user_url_or_create_book_order(callback: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    if data['product'] == 'album':
+        await state.set_state(UserData.url_state)
+        await callback.message.edit_text(text=phrases.order_phrases["drive_link"])
+    elif data['product'] == 'book':
+        order_id = BotDB().create_order(user_id=callback.from_user.id, product=data['product'])
+        text = BotDB().order_message_for_admin(order_id=order_id, product=data['product'])
+
+        await sol_bot.send_message(chat_id=os.getenv('GROUP_ID'), text=f'<b>[CREATED]</b>'
+                                                                       f'\n{text}')
+
+        await callback.message.edit_text(text=f'Great! Order number: <b>{order_id}</b>')
+        await callback.message.answer(text=phrases.order_phrases['book_payment'],
+                                      reply_markup=callback_buttons.payment_kb())
+        await state.clear()
+
     await callback.answer()
 
 
@@ -163,38 +177,31 @@ async def get_user_url(message: types.Message, state: FSMContext):
 async def confirm_or_change_url(callback: types.CallbackQuery, state: FSMContext):
     if callback.data == 'Next url':
         url = await state.get_data()
-        order_id = BotDB().create_order(user_id=callback.from_user.id, url=url['url'])
+        order_id = BotDB().create_order(user_id=callback.from_user.id, product='album', url=url['url'])
+        text = BotDB().order_message_for_admin(order_id=order_id, product='album')
 
-        text = BotDB().order_message_for_admin(order_id=order_id)
-
-        await sol_bot.send_message(chat_id=os.getenv('GROUP_ID'), text=f'[CREATED]'
+        await sol_bot.send_message(chat_id=os.getenv('GROUP_ID'), text=f'<b>[CREATED]</b>'
                                                                        f'\n{text}')
 
         await callback.message.edit_text(text=f'Great! Order number: <b>{order_id}</b>')
-        await callback.message.answer(text=f'Right now, we have to decide how are you going '
-                                           f'to pay for your photo album.',
+        await callback.message.answer(text=phrases.order_phrases['album_payment'],
                                       reply_markup=callback_buttons.payment_kb())
-        await callback.answer()
+        await state.clear()
 
     if callback.data == 'Change url':
         await state.set_state(UserData.url_state)
         await callback.message.edit_text(
-            text='No problem.'
-                 '\n\nRight now, could you send me the public link '
-                 'for your google drive with your photos (Maximum 30 photos).')
-        await callback.answer()
+            text=f'No problem.'
+                 f'\n\n{phrases.order_phrases["drive_link"]}')
+    await callback.answer()
 
 
 @router.callback_query(F.data.in_({'In Cash', 'Bank Account'}))
 async def choose_payment_method(callback: types.CallbackQuery):
     if callback.data == 'In Cash':
-        await callback.message.edit_text(text='To pay in cash, please contact with @yusungchoi')
+        await callback.message.edit_text(text=phrases.order_phrases['payment_cash'])
     if callback.data == 'Bank Account':
-        await callback.message.edit_text(text='You can just transfer your money to our photo club bank '
-                                              'account.'
-                                              '\n\nWhen you upload it, '
-                                              'please send to @yusungchoi the screenshot of your '
-                                              'transaction.')
+        await callback.message.edit_text(text=phrases.order_phrases['payment_bank'])
         await callback.message.answer(text='<b>64791074681307</b>')
     await callback.message.answer(text='👌', reply_markup=default_buttons.main_menu_kb())
     await callback.answer()

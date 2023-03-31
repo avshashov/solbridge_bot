d9cb344c79fd244790c59b61bffd9b1bbdce2710
Artem Shashov <tauquantum@gmail.com>
2023-03-31 18:25:36 +1000

    added orders buttons

--- keybords/callback_buttons.py
+++ keybords/callback_buttons.py
@@ -1,6 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+from solbot_db.db_orm import BotDB
+
 
 def edit_message_kb() -> InlineKeyboardMarkup:
     buttons = [
@@ -99,9 +101,9 @@ def change_data_kb() -> InlineKeyboardMarkup:
 
 def choose_user_data_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.add(InlineKeyboardButton(text='Name', callback_data='Name'))
-    builder.add(InlineKeyboardButton(text='Email', callback_data='Email'))
-    builder.add(InlineKeyboardButton(text='Instagram', callback_data='Instagram'))
+    builder.add(InlineKeyboardButton(text='Name', callback_data='name'))
+    builder.add(InlineKeyboardButton(text='Email', callback_data='email'))
+    builder.add(InlineKeyboardButton(text='Instagram', callback_data='instagram'))
     builder.add(InlineKeyboardButton(text='Back', callback_data='Back change'))
     builder.adjust(1)
     return builder.as_markup()
@@ -116,7 +118,13 @@ def change_url_kb() -> InlineKeyboardMarkup:
 
 def order_photo_album_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.add(InlineKeyboardButton(text='Order the Photo Album', callback_data='Order the Photo Album'))
+    builder.add(InlineKeyboardButton(text='Order the Photo Album', callback_data='album'))
+    return builder.as_markup()
+
+
+def order_book_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(text='Order the PCS Book', callback_data='book'))
     return builder.as_markup()
 
 
@@ -124,4 +132,24 @@ def payment_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.add(InlineKeyboardButton(text='In Cash', callback_data='In Cash'))
     builder.add(InlineKeyboardButton(text='Bank Account', callback_data='Bank Account'))
+    return builder.as_markup()
+
+
+def unpaid_orders_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    unpaid_orders = BotDB().get_unpaid_orders()
+
+    for order in unpaid_orders:
+        builder.add(InlineKeyboardButton(text=order[0], callback_data=f'order {order[0]}'))
+
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+def order_unpaid_more_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(text='✅ Approve the order', callback_data='Approve'))
+    builder.add(InlineKeyboardButton(text='❌ Cancel the order', callback_data='Cancel'))
+    builder.add(InlineKeyboardButton(text='↩️ Back to orders', callback_data='Back to orders'))
+    builder.adjust(2)
     return builder.as_markup()

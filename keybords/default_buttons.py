858925e0d26bd1f1abf8c2f6e1d613aa644cf180
Artem Shashov <tauquantum@gmail.com>
2023-04-03 19:09:28 +1000

    removed buttons for viewing order statuses

--- keybords/default_buttons.py
+++ keybords/default_buttons.py
@@ -12,45 +12,6 @@ def main_menu_kb() -> ReplyKeyboardMarkup:
     return kb.as_markup(resize_keyboard=True, one_time_keyboard=False)
 
 
-def admin_menu_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text='Orders')
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-def orders_category_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text='Photo Albums')
-    kb.button(text='Books')
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-def photo_album_status_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text='Open')
-    kb.button(text='Closed')
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-def order_is_open_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text='Paid')
-    kb.button(text='Unpaid')
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
-def order_closed_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    kb.button(text='Cancelled')
-    kb.button(text='Completed')
-    kb.adjust(1)
-    return kb.as_markup(resize_keyboard=True, one_time_keyboard=True)
-
-
 def cancel_kb() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text='Cancel')

4e5a38e481f95111d63f25987d35238fb160bceb
Artem Shashov <tauquantum@gmail.com>
2023-04-16 18:14:21 +1000

    added try/except (TelegramForbiddenError), renamed env params

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,7 @@
 import os
 
 from aiogram import F, Router, types
+from aiogram.exceptions import TelegramForbiddenError
 from aiogram.filters import Command
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -18,19 +19,23 @@ async def publish_post(callback: types.CallbackQuery):
     text = callback.message.html_text
 
     if callback.message.photo:
-        await sol_bot.send_photo(chat_id=os.getenv('MAIN_GROUP_ID'), message_thread_id=os.getenv('TOPIC_ID'),
+        await sol_bot.send_photo(chat_id=os.getenv('TOPIC_GROUP_ID'), message_thread_id=os.getenv('TOPIC_THREAD_ID'),
                                  photo=callback.message.photo[-1].file_id, caption=text)
     elif callback.message.document:
-        await sol_bot.send_document(chat_id=os.getenv('MAIN_GROUP_ID'), message_thread_id=os.getenv('TOPIC_ID'),
+        await sol_bot.send_document(chat_id=os.getenv('TOPIC_GROUP_ID'), message_thread_id=os.getenv('TOPIC_THREAD_ID'),
                                     document=callback.message.document.file_id, caption=text)
 
     await callback.message.delete_reply_markup()
     await callback.message.edit_caption(caption=f'{text}\n\n<b>[Approved]</b>')
 
-    user_id = callback.data.split()[1]
-    # try/except
-    await sol_bot.send_message(chat_id=user_id, text='Congratulations! your photo has been approved by the club\'s '
-                                                     'moderation!')
+    try:
+        user_id = callback.data.split()[1]
+        await sol_bot.send_message(chat_id=user_id, text='Congratulations! your photo has been approved by the club\'s '
+                                                         'moderation!')
+    except IndexError:
+        pass
+    except TelegramForbiddenError:
+        pass
     await callback.answer()
 
 
@@ -39,23 +44,25 @@ async def publish_to_collection(callback: types.CallbackQuery):
     text = callback.message.html_text
 
     if callback.message.photo:
-        await sol_bot.send_photo(chat_id=os.getenv('MAIN_GROUP_ID'), message_thread_id=os.getenv('TOPIC_ID'),
+        await sol_bot.send_photo(chat_id=os.getenv('TOPIC_GROUP_ID'), message_thread_id=os.getenv('TOPIC_THREAD_ID'),
                                  photo=callback.message.photo[-1].file_id, caption=text)
-        await sol_bot.send_photo(chat_id=os.getenv('CHANNEL_ID'), photo=callback.message.photo[-1].file_id,
+        await sol_bot.send_photo(chat_id=os.getenv('COLLECTION_CHANNEL_ID'), photo=callback.message.photo[-1].file_id,
                                  caption=text)
     elif callback.message.document:
-        await sol_bot.send_document(chat_id=os.getenv('MAIN_GROUP_ID'), message_thread_id=os.getenv('TOPIC_ID'),
+        await sol_bot.send_document(chat_id=os.getenv('TOPIC_GROUP_ID'), message_thread_id=os.getenv('TOPIC_THREAD_ID'),
                                     document=callback.message.document.file_id, caption=text)
-        await sol_bot.send_document(chat_id=os.getenv('CHANNEL_ID'), document=callback.message.document.file_id,
+        await sol_bot.send_document(chat_id=os.getenv('COLLECTION_CHANNEL_ID'), document=callback.message.document.file_id,
                                     caption=text)
 
     await callback.message.delete_reply_markup()
     await callback.message.edit_caption(caption=f'{text}\n\n<b>[Published to Collection]</b>')
 
     user_id = callback.data.split()[1]
-    # try/except
-    await sol_bot.send_message(chat_id=user_id, text='Awesome! Your photo is so beautiful that'
-                                                     ' we immediately published it in our official collection')
+    try:
+        await sol_bot.send_message(chat_id=user_id, text='Awesome! Your photo is so beautiful that '
+                                                         'we immediately published it in our official collection')
+    except TelegramForbiddenError:
+        pass
     await callback.answer()
 
 

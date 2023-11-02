8d6e69c2ff92b951464440239c42b4a5b705aa6e
Artem Shashov <tauquantum@gmail.com>
2023-11-02 19:14:09 +1000

    added settings from config, decomposed main func, added def (init_argparser and init_dispatcher)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import argparse
 import asyncio
 import logging
-import os
 
 from aiogram import Dispatcher
 from aiogram.types import BotCommand
@@ -8,16 +8,29 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from bot import SingleBot
-from database.models import Base
+from config import SettingsDB, SettingsScheduler
+from database.models import create_tables
 import job
 from handlers.photo_uploader import router as uploader_router
 from handlers.admin import router as admin_router
 from handlers.orders import router as orders_router
 from middlewares.session_db import SessionMiddleware
 
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-sol_bot = SingleBot()
+def init_argparser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+def init_dispatcher(sessionmaker: async_sessionmaker) -> Dispatcher:
+    dp = Dispatcher()
+    dp.update.middleware.register(SessionMiddleware(sessionmaker))
+    dp.include_router(uploader_router)
+    dp.include_router(admin_router)
+    dp.include_router(orders_router)
+    return dp
 
 
 async def set_commands(bot):
@@ -28,25 +41,27 @@ async def set_commands(bot):
     await bot.set_my_commands(commands)
 
 
-async def main(bot):
-    DSN = f'postgresql+asyncpg://{os.getenv("USER_DB")}:{os.getenv("PASSWORD_DB")}@{os.getenv("HOST_DB")}:5432' \
-          f'/{os.getenv("NAME_DB")}'
-    engine = create_async_engine(DSN, echo=False, future=True)
-    session = async_sessionmaker(engine, expire_on_commit=False)
+async def main():
+    bot = SingleBot()
+    args = init_argparser()
+    debug_depth = logging.DEBUG if args.debug else logging.ERROR
+    logging.basicConfig(level=debug_depth, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    settings_db = SettingsDB()
+    engine = create_async_engine(settings_db.pg_dsn, echo=False, future=True)
+    sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    await create_tables(engine)
 
-    dp = Dispatcher()
-    dp.update.middleware.register(SessionMiddleware(session))
-    dp.include_router(uploader_router)
-    dp.include_router(admin_router)
-    dp.include_router(orders_router)
+    dp = init_dispatcher(sessionmaker)
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(job.send_post, trigger='cron',
-                      hour='10-21', minute='*/20', timezone='Asia/Seoul',
-                      args=(session,))
+    settings_scheduler = SettingsScheduler()
+    scheduler.add_job(job.send_post,
+                      trigger=settings_scheduler.trigger,
+                      hour=settings_scheduler.hour,
+                      minute=settings_scheduler.minute,
+                      timezone=settings_scheduler.timezone,
+                      args=(sessionmaker,))
     await bot.delete_webhook(drop_pending_updates=True)
     await set_commands(bot)
     scheduler.start()
@@ -54,4 +69,4 @@ async def main(bot):
 
 
 if __name__ == '__main__':
-    asyncio.run(main(sol_bot))
+    asyncio.run(main())

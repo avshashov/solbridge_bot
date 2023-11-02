d2ac9f762cb9a8ada28d90ef0cca9611beedd513
Artem Shashov <tauquantum@gmail.com>
2023-11-02 19:24:54 +1000

    refactored code

--- job.py
+++ job.py
@@ -2,57 +2,48 @@ import os
 from random import choice
 
 from aiogram.exceptions import TelegramForbiddenError
-from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from bot import SingleBot
 from database.queue import get_post_from_queue, change_post_status
 import phrases
 
-load_dotenv()
 sol_bot = SingleBot()
 
 
-async def send_post(sessionmaker):
+async def send_post(sessionmaker: async_sessionmaker) -> None:
     post = await get_post_from_queue(sessionmaker)
 
-    if post:
-        id, user_id, type, = post.id, post.user_id, post.type,
-        text, file_id, target = post.text, post.file_id, post.target
+    if not post:
+        return
 
-        if target:
-            if type == 'photo':
-                await sol_bot.send_photo(chat_id=os.getenv('TOPIC_GROUP_ID'),
-                                         message_thread_id=os.getenv('TOPIC_THREAD_ID'),
-                                         photo=file_id, caption=text)
-                await sol_bot.send_photo(chat_id=os.getenv('COLLECTION_CHANNEL_ID'),
-                                         photo=file_id,
-                                         caption=text)
-            if type == 'document':
-                await sol_bot.send_document(chat_id=os.getenv('TOPIC_GROUP_ID'),
-                                            message_thread_id=os.getenv('TOPIC_THREAD_ID'),
-                                            document=file_id, caption=text)
-                await sol_bot.send_document(chat_id=os.getenv('COLLECTION_CHANNEL_ID'),
-                                            document=file_id,
-                                            caption=text)
-            try:
-                await sol_bot.send_message(chat_id=user_id,
-                                           text=choice(phrases.user_notification_phrases['Collection']))
-            except TelegramForbiddenError:
-                pass
+    if post.type == 'photo':
+        await sol_bot.send_photo(chat_id=os.getenv('TOPIC_GROUP_ID'),
+                                 message_thread_id=os.getenv('TOPIC_THREAD_ID'),
+                                 photo=post.file_id,
+                                 caption=post.text)
+        if post.target:
+            await sol_bot.send_photo(chat_id=os.getenv('COLLECTION_CHANNEL_ID'),
+                                     photo=post.file_id,
+                                     caption=post.text)
+    elif post.type == 'document':
+        await sol_bot.send_document(chat_id=os.getenv('TOPIC_GROUP_ID'),
+                                    message_thread_id=os.getenv('TOPIC_THREAD_ID'),
+                                    document=post.file_id,
+                                    caption=post.text)
+        if post.target:
+            await sol_bot.send_document(chat_id=os.getenv('COLLECTION_CHANNEL_ID'),
+                                        document=post.file_id,
+                                        caption=post.text)
 
-        if not target:
-            if type == 'photo':
-                await sol_bot.send_photo(chat_id=os.getenv('TOPIC_GROUP_ID'),
-                                         message_thread_id=os.getenv('TOPIC_THREAD_ID'),
-                                         photo=file_id, caption=text)
-            if type == 'document':
-                await sol_bot.send_document(chat_id=os.getenv('TOPIC_GROUP_ID'),
-                                            message_thread_id=os.getenv('TOPIC_THREAD_ID'),
-                                            document=file_id, caption=text)
+    try:
+        if post.target:
+            await sol_bot.send_message(chat_id=post.user_id,
+                                       text=choice(phrases.user_notification_phrases['Collection']))
+        else:
+            await sol_bot.send_message(chat_id=post.user_id,
+                                       text=choice(phrases.user_notification_phrases['Approve']))
+    except TelegramForbiddenError:
+        pass
 
-            try:
-                await sol_bot.send_message(chat_id=user_id, text=choice(phrases.user_notification_phrases['Approve']))
-            except TelegramForbiddenError:
-                pass
-
-        await change_post_status(sessionmaker, id=id)
+    await change_post_status(sessionmaker, id=post.id)
